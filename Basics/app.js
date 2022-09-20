6f3131451dc151a404d2218fd4fda3493bed2f97
SHresTho12 <talimul12bari@gmail.com>
2022-09-20 08:33:49 +0600

    Express used to create a server that sends respose of hello world

--- Basics/app.js
+++ Basics/app.js
@@ -1,14 +1,13 @@
-// Load HTTP module
-const http = require("http");
-
-const hostname = "127.0.0.1";
+const express = require("express"); 
+const app = express(); 
 const port = 3000;
 
 
-const server = http.createServer(function (req, res) {
- 
-  res.writeHead(200, { "Content-Type": "text/plain" });
-
-  res.end("Hello World\n");
+app.get("/", function (req, res) {
+  res.send("Hello World!");
 });
 
+app.listen(port, function () {
+  //starts up the server on a specified port ('3000')
+  console.log(`Example app listening on port ${port}!`);
+});
