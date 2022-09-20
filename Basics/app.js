f537f9e24e0a4e1fda1adbe8a21471d5419e23d7
SHresTho12 <talimul12bari@gmail.com>
2022-09-20 13:25:33 +0600

    form route and form controller added

--- Basics/app.js
+++ Basics/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express(); 
 const port = 3000;
 const router = require("./router");
+const bodyParser = require('body-parser')
 app.use(express.static("public"));
 app.use(router); 
 app.set("view engine", "ejs"); 
--- Basics/router.js
+++ Basics/router.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const { getCV } = require("./controllers/CvController");
+const {form } = require("./controllers/FormController");
 
 const fs = require("fs");
 
 router.get("/", getCV);
+router.get("/info", form);
 
 module.exports = router;
